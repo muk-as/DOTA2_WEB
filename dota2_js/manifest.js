5caa8b5b9905c83985f1df791099e5133428524b
muk-as <muk-as@yandex.ru>
2023-12-18 21:49:53 +0300

    dota2_js | 4 files | https://t.me/DOTA_DM

--- dota2_js/manifest.js
+++ dota2_js/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "8592085";
+var CLSTAMP = "8594561";
 (() => {
   "use strict";
   var e,
