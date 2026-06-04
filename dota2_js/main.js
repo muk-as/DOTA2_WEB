22504e7ea8e0d88238ad0b4d4ff261f85544d614
muk-as <muk-as@yandex.ru>
2026-06-04 20:19:27 +0300

    dota2_js | 3 files | https://t.me/DOTA_DM

--- dota2_js/main.js
+++ dota2_js/main.js
@@ -73248,7 +73248,7 @@
         const m = {
           project_id: 8,
           published_version: 10,
-          additional_data: [10],
+          additional_data: [20],
           data: {
             data: [
               {
